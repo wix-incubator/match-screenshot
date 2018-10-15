44d117e42a6c18a1028d7ca6f89ecb07481b709a
liorur <lioru@wix.com>
2018-10-15 13:23:24 +0300

    gracefully handle absence of EYES_API_KEY (#31)

--- tests/toMatchScreenshotChai.test.js
+++ tests/toMatchScreenshotChai.test.js
@@ -1,19 +1,24 @@
 const execa = require('execa');
+const {conditionalTest} = require('./toMatchScreenshot.driver.js');
 
 describe('EYES', () => {
   jest.setTimeout(30000);
   describe('Log', () => {
-    test('should log after eyes success', async () => {
-      const browserWSEndpoint = global.browser.wsEndpoint();
-      const res = await execa('node', [
-        require.resolve('mocha/bin/mocha'),
-        require.resolve('./__fixtures__/test.mocha'),
-        require.resolve('../lib/chai'),
-        browserWSEndpoint,
-      ]);
-      expect(res.stdout).toContain(
-        'eyes comparison succeed for test "should work with chai v1.0.0"',
-      );
-    });
+    conditionalTest(
+      'should log after eyes success',
+      async () => {
+        const browserWSEndpoint = global.browser.wsEndpoint();
+        const res = await execa('node', [
+          require.resolve('mocha/bin/mocha'),
+          require.resolve('./__fixtures__/test.mocha'),
+          require.resolve('../lib/chai'),
+          browserWSEndpoint,
+        ]);
+        expect(res.stdout).toContain(
+          'eyes comparison succeed for test "should work with chai v1.0.0"',
+        );
+      },
+      process.env.EYES_API_KEY,
+    );
   });
 });
--- tests/toMatchScreenshotJest.test.js
+++ tests/toMatchScreenshotJest.test.js
@@ -1,76 +1,114 @@
-const execa = require('execa');
-const dargs = require('dargs');
+const {conditionalTest, executeTest} = require('./toMatchScreenshot.driver.js');
 
 describe('EYES', () => {
   jest.setTimeout(30000);
 
-  describe('Log', () => {
-    test('should log after eyes success', async () => {
-      const options = {
-        config: require.resolve('./__fixtures__/jest-default/conf.json'),
-      };
+  const eyesApiKey = process.env.EYES_API_KEY;
 
-      const res = await execa('node', [
-        require.resolve('jest/bin/jest'),
-        require.resolve('./__fixtures__/jest-default/test.jest'),
-        dargs(options),
-      ]);
+  if (!process.env.EYES_API_KEY) {
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      '>>> EYES_API_KEY is missing. For all the tests to run please add .env file to the root of the project with EYES_API_KEY=<your key here> <<<',
+    );
+  }
 
-      expect(res.stdout).toContain(
-        'eyes comparison succeed for test "should work with jest v1.0.0"',
+  describe('when EYES_API_KEY is missing', () => {
+    beforeAll(() => {
+      process.env.EYES_API_KEY = '';
+    });
+
+    test('should return success without accessing eyes', async () => {
+      await global.page.setContent('<div>Hello World 123</div>');
+      const screenshot1 = await global.page.screenshot({fullpage: true});
+      let error;
+      try {
+        await expect(screenshot1).toMatchScreenshot({
+          key: 'Missing API key',
+        });
+
+        await global.page.setContent('<div>Hello World 1234</div>');
+        const screenshot2 = await global.page.screenshot({fullpage: true});
+
+        await expect(screenshot2).toMatchScreenshot({
+          key: 'Missing API key',
+        });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).toBeUndefined();
+      expect(await executeTest()).toContain(
+        'EYES_API_KEY not found. Eyes comparison skipped for test "should work with jest v1.0.0"',
       );
     });
+
+    afterAll(() => {
+      process.env.EYES_API_KEY = eyesApiKey;
+    });
+  });
+
+  describe('Log', () => {
+    conditionalTest(
+      'should log after eyes success',
+      async () => {
+        expect(await executeTest()).toContain(
+          'eyes comparison succeed for test "should work with jest v1.0.0"',
+        );
+      },
+      eyesApiKey,
+    );
   });
 
   describe('should works with configurations', () => {
-    test('should log after eyes success', async () => {
-      const options = {
-        config: require.resolve('./__fixtures__/jest-with-config/conf.json'),
-      };
-
-      const res = await execa('node', [
-        require.resolve('jest/bin/jest'),
-        require.resolve('./__fixtures__/jest-with-config/test.jest'),
-        dargs(options),
-      ]);
-
-      expect(res.stdout).toContain(
-        'eyes comparison succeed for test "should work with jest v1.0.0"',
-      );
-    });
+    conditionalTest(
+      'should log after eyes success',
+      async () => {
+        expect(await executeTest()).toContain(
+          'eyes comparison succeed for test "should work with jest v1.0.0"',
+        );
+      },
+      eyesApiKey,
+    );
   });
 
-  test('should support a new baseline', async () => {
-    await global.page.setContent('<div>Hello World</div>');
-    const screenshot = await global.page.screenshot({fullpage: true});
-    await expect(screenshot).toMatchScreenshot({
-      key: 'Hello World',
-      version: 'v1.0.0',
-    });
-    await global.page.setContent('<div>Hello World 123</div>');
-    const screenshot2 = await global.page.screenshot({fullpage: true});
-    await expect(screenshot2).toMatchScreenshot({
-      key: 'Hello World',
-      version: 'v1.0.1',
-    });
-  });
-
-  test('should fail', async () => {
-    await global.page.setContent('<div>Hello World</div>');
-    const screenshot = await global.page.screenshot({fullpage: true});
-    await expect(screenshot).toMatchScreenshot({
-      key: 'Failing Hello World',
-    });
-    await global.page.setContent('<div>Hello World 123</div>');
-    const screenshot2 = await global.page.screenshot({fullpage: true});
-    let error;
-    try {
+  conditionalTest(
+    'should support a new baseline',
+    async () => {
+      await global.page.setContent('<div>Hello World</div>');
+      const screenshot = await global.page.screenshot({fullpage: true});
+      await expect(screenshot).toMatchScreenshot({
+        key: 'Hello World',
+        version: 'v1.0.0',
+      });
+      await global.page.setContent('<div>Hello World 123</div>');
+      const screenshot2 = await global.page.screenshot({fullpage: true});
       await expect(screenshot2).toMatchScreenshot({
+        key: 'Hello World',
+        version: 'v1.0.1',
+      });
+    },
+    eyesApiKey,
+  );
+
+  conditionalTest(
+    'should fail',
+    async () => {
+      await global.page.setContent('<div>Hello World</div>');
+      const screenshot = await global.page.screenshot({fullpage: true});
+      await expect(screenshot).toMatchScreenshot({
         key: 'Failing Hello World',
       });
-    } catch (e) {
-      error = e;
-    }
-    expect(error).toBeTruthy();
-  });
+      await global.page.setContent('<div>Hello World 123</div>');
+      const screenshot2 = await global.page.screenshot({fullpage: true});
+      let error;
+      try {
+        await expect(screenshot2).toMatchScreenshot({
+          key: 'Failing Hello World',
+        });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).toBeTruthy();
+    },
+    eyesApiKey,
+  );
 });
